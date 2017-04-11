1e672b9b997d850d1e68e00ac5210b80a00a410e
Chaz Henricks <chazhenricks@gmail.com>
2017-04-11 14:33:22 -0500

    js file functional

--- js/main.js
+++ js/main.js
@@ -9,29 +9,47 @@ var resultNum;
 
 // !check if the test number has a remainder that is not a integer
 
-// if test number has a ramainder that is an interger, test next number in array
+// !if test number has a ramainder that is an interger, test next number in array
 
-//if test number has a remainder that is not an integer, break the loop and add 1 to your test number and start again.
+//!if test number has a remainder that is not an integer, break the loop and add 1 to your test number and start again.
 
-//if test number is successfully divisible by all numbers in array log the value of that number
-
-
-/*
-superNumber.forEach(function(element){
-  resultNum = testNum / element;
-  if(Number.isInteger(resultNum)){
-    console.log("remainder is an integer", resultNum);
-  }else{
-    console.log("remainder is not an integer", resultNum);
-  }
-});
-*/
+//!if test number is successfully divisible by all numbers in array log the value of that number
 
 
-superNumber.forEach(function(element){
-  resultNum = testNum/element;
-  while (Number.isInteger(resultNum)){
+function testThisNumber(){
+  superNumber.forEach(function(element){
+    resultNum = testNum / element;
+    switch (Number.isInteger(resultNum)){
+      case false:
+      testNum++;
+      testThisNumber();
+      break;
 
-  }
+      default:
+      break;
+    };
+  })
+}
 
-})
+testThisNumber();
+console.log("The smallest number divisible by digits 1-10 is " + testNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
